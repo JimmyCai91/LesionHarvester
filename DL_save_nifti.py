0361d1e5ef401b142dc34e9cc38022ba0742331c
JimCai91 <jinzheng.cai@panacea-ai.org>
2020-01-20 23:23:48 -0500

    organize data for TMI submission

---
+++ DL_save_nifti.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+"""
+Ke Yan
+Imaging Biomarkers and Computer-Aided Diagnosis Laboratory
+National Institutes of Health Clinical Center
+May 2018
+THIS SOFTWARE IS PROVIDED BY THE AUTHOR(S) ``AS IS'' AND ANY EXPRESS OR
+IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+IN NO EVENT SHALL THE AUTHOR(S) BE LIABLE FOR ANY DIRECT, INDIRECT,
+INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+"""
+A simple demo to load 2D 16-bit slices from DeepLesion and save to 3D nifti volumes.
+The nifti volumes can be viewed in software such as 3D slicer and ITK-SNAP.
+"""
+
+
+import numpy as np
+import nibabel as nib
+import os
+import cv2
+import csv
+
+
+dir_in = 'Images_png'
+dir_out = 'Images_nifti'
+out_fmt = '%s_%03d-%03d.nii.gz'  # format of the nifti file name to output
+info_fn = 'DL_info.csv'  # file name of the information file
+
+
+def slices2nifti(ims, fn_out, spacing):
+    """save 2D slices to 3D nifti file considering the spacing"""
+    if len(ims) < 300:  # cv2.merge does not support too many channels
+        V = cv2.merge(ims)
+    else:
+        V = np.empty((ims[0].shape[0], ims[0].shape[1], len(ims)))
+        for i in range(len(ims)):
+            V[:, :, i] = ims[i]
+
+    # the transformation matrix suitable for 3D slicer and ITK-SNAP
+    T = np.array([[0, -spacing[1], 0, 0], [-spacing[0], 0, 0, 0], [0, 0, -spacing[2], 0], [0, 0, 0, 1]])
+    img = nib.Nifti1Image(V, T)
+    path_out = os.path.join(dir_out, fn_out)
+    nib.save(img, path_out)
+    print fn_out, 'saved'
+
+
+def load_slices(dir, slice_idxs):
+    """load slices from 16-bit png files"""
+    slice_idxs = np.array(slice_idxs)
+    assert np.all(slice_idxs[1:] - slice_idxs[:-1] == 1)
+    ims = []
+    for slice_idx in slice_idxs:
+        fn = '%03d.png' % slice_idx
+        path = os.path.join(dir_in, dir, fn)
+        im = cv2.imread(path, -1)  # -1 is needed for 16-bit image
+        assert im is not None, 'error reading %s' % path
+        print 'read', path
+
+        # the 16-bit png file has a intensity bias of 32768
+        ims.append((im.astype(np.int32) - 32768).astype(np.int16))
+    return ims
+
+
+def read_DL_info():
+    """read spacings and image indices in DeepLesion"""
+    spacings = []
+    idxs = []
+    with open(info_fn, 'rb') as csvfile:
+        reader = csv.reader(csvfile)
+        rownum = 0
+        for row in reader:
+            if rownum == 0:
+                header = row
+                rownum += 1
+            else:
+                idxs.append([int(d) for d in row[1:4]])
+                spacings.append([float(d) for d in row[12].split(',')])
+
+    idxs = np.array(idxs)
+    spacings = np.array(spacings)
+    return idxs, spacings
+
+
+if __name__ == '__main__':
+    idxs, spacings = read_DL_info()
+    if not os.path.exists(dir_out):
+        os.mkdir(dir_out)
+    img_dirs = os.listdir(dir_in)
+    img_dirs.sort()
+    for dir1 in img_dirs:
+        # find the image info according to the folder's name
+        idxs1 = np.array([int(d) for d in dir1.split('_')])
+        i1 = np.where(np.all(idxs == idxs1, axis=1))[0]
+        spacings1 = spacings[i1[0]]
+
+        fns = os.listdir(os.path.join(dir_in, dir1))
+        slices = [int(d[:-4]) for d in fns if d.endswith('.png')]
+        slices.sort()
+
+        # Each folder contains png slices from one series (volume)
+        # There may be several sub-volumes in each volume depending on the key slices
+        # We group the slices into sub-volumes according to continuity of the slice indices
+        groups = []
+        for slice_idx in slices:
+            if len(groups) != 0 and slice_idx == groups[-1][-1]+1:
+                groups[-1].append(slice_idx)
+            else:
+                groups.append([slice_idx])
+
+        for group in groups:
+            # group contains slices indices of a sub-volume
+            ims = load_slices(dir1, group)
+            fn_out = out_fmt % (dir1, group[0], group[-1])
+            slices2nifti(ims, fn_out, spacings1)
--- README.md
+++ README.md
@@ -1,3 +1,40 @@
-# DeepLesionAnnotation
+# DeepLesion Annotation  
+>To harvest lesions from the [DeepLesion](https://nihcc.app.box.com/v/DeepLesion) dataset, we randomly select $844$ volumes from the original $14\,075$ training CT. These are then annotated by a board-certified radiologist. Of these, we select $744$ as $V_{M}$ ($5.3\%$) and leave another $100$ as an evaluation set for lesion harvesting. This latter subset, denoted $V_{H}^{test}$, is treated identically at $V_{H}$, meaning the algorithm only sees the original DeepLesion RECIST marks. After convergence, we can measure the precision and recall of the harvested lesions. In addition, we later measure detection performance on systems trained on our harvested lesions by also fully annotating $1,071$ of the testing CT volumes. These volumes, denoted $V_{D}^{test}$, are never seen in our harvesting framework. 
 
-Annotation of DeepLesion
+
+## Data Structure
+>|- annotation  
+|- |- RECIST-Box-Train.pkl # In the official training split, boxes converted from RECIST marks.  
+|- |- RECIST-Box-Valid.pkl # In the official validating split, boxes converted from RECIST marks.  
+|- |- RECIST-Box-Test.pkl  # In the official testing split, boxes converted from RECIST marks.  
+|- |- Revised-Train744.pkl # $V_{M}$  
+|- |- Revised-Train100.pkl # $V_{H}^{test}$  
+|- |- Revised-Test1071.pkl # $V_{D}^{test}$  
+|- |- 3D-Box.pkl # 3D bounding box annotation of volumes selected from $V_{D}^{test}$.  
+|  
+|- DL_save_nifti.py 
+
+## Usage 
+1. Convert data formation from *png* to *nifti*:
+    ```python 
+    python DL_save_nifti.py 
+    ```
+    It generates CT subvolumes named in the format of *PatientID_StudyID_ScanID_StartingSliceID_EndingSliceID.nii.gz*, for example "001344_01_01_012-024.nii.gz". The python script **DL_save_nifti.py** is adapted from [DeepLesion](https://nihcc.app.box.com/v/DeepLesion).
+
+2. Read 2D box annotation
+    ```python
+    import pickle 
+    annotation = pickle.load(open(dir_to_annotation_file, 'rb'))
+    print(annotation['001344_01_01_012-024']) 
+    # {6: [[215.383, 176.983, 267.122, 220.374, 6.0, 6.0]]} 
+    ```
+    Annotation of each CT volume is storied as **python dictionary**. The key-value pair is in the format of *{key: [[$x_{min}$, $y_{min}$, $x_{max}$, $y_{max}$, $z_{min}$, $z_{max}$]]}*. 
+
+3. Read 3D box annotation
+    ```python
+    import pickle 
+    annotation = pickle.load(open(dir_to_annotation_file, 'rb'))
+    print(annotation['000001_01_01_103-115'])
+    # [[224.11279125118176, 92.50398222171341, 241.87335205606877, 113.86161863265343, 5.0, 6.0], [234.21612865759417, 78.1168292149383, 256.46265925305823, 104.75412242792794, 5.0, 6.0]]
+    ```
+    Annotation of each CT volume is storied as **python list**. Each list element is in the format of *[$x_{min}$, $y_{min}$, $x_{max}$, $y_{max}$, $z_{min}$, $z_{max}$]*.
